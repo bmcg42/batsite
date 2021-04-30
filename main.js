124dd919fd5bc1b7f2a41544817b0ce0c96471b2
bmcg42 <80113628+bmcg42@users.noreply.github.com>
2021-04-30 10:08:41 -0400

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-function spectro(){
-    //var f = eel.spectrogram(document.getElementById("input").files[0])
-    //image(f)
-    output_filename(document.getElementById("filename").value)
-}
-
-function image(filename){
-    document.getElementById("output").src=filename
-}
